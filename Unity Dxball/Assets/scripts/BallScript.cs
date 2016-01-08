e997fc6df58291dfd0ab07d01a7accab52add477
sdbibon <soumikdas25@yahoo.com>
2016-01-08 11:32:59 +0600

    A good looking graphics 3D dx-ball game was made using Unity 3D
    Graphics is nice, sound quality is mind blowing, you will surely like
    the game.

---
+++ Unity Dxball/Assets/scripts/BallScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        rigidbody.AddForce(0, 250f, 100f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 3.8 && transform.position.x < 4)
+        {
+             
+        }
+	
+	}
+}
---
+++ Unity Dxball/Assets/scripts/PaddleScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(10f * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+        
+      
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        foreach (ContactPoint contact in col.contacts)
+        {
+            if (contact.thisCollider == collider)
+            {
+                float english = contact.point.x - transform.position.x;
+                contact.otherCollider.rigidbody.AddForce(300f * english, 0, 0);
+            }
+        }
+
+    }
+}
